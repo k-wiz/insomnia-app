5613c89b63ffccbb625596dfe5fc506ae2fadb1f
kelli <kwisuri@gmail.com>
2016-05-10 17:50:14 -0700

    Completed type-casting; new records can now be added to the db.

--- server.py
+++ server.py
@@ -2,11 +2,9 @@
 
 from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-
 from jinja2 import StrictUndefined
-
-#NEED TO IMPORT MODELS
 from model import connect_to_db, db, User, Entry
+from datetime import datetime, time, date
 
 app = Flask(__name__)
 #WHAT IS THIS?
@@ -28,44 +26,64 @@ def dashboard():
     """Display user's dashboard."""
 
     #entry_id auto assigned
-    user_id = 1 #get id from session
-    #date = datetime.datetime.now()
-    hours_sleep = request.form.get("hours_sleep")
-    print "HOURS SLEEP", hours_sleep
+
+    #user_id hardcoded now; get id from session once login setup
+    user_id = 1
+
+    date = datetime.now()
+
+    hours_sleep = float(request.form.get("hours_sleep"))
+
     insomnia = request.form.get("insomnia")
-    print "Insomnia", insomnia
+    if insomnia == 'True':
+        insomnia = True
+    else:
+        insomnia = False
+
     insom_type = request.form.get("insom_type")
-    print "TYPE", insom_type
-    insom_severity = request.form.get("insom_severity")
-    print "SEVERITY", insom_severity
+
+    insom_severity = int(request.form.get("insom_severity"))
+
     alcohol = request.form.get("alcohol")
-    print "alcohol", alcohol, type(alcohol)
+    if alcohol == 'True':
+        alcohol = True
+    else:
+        alcohol = False
+
     caffeine = request.form.get("caffeine")
-    print "caffeine", caffeine
+    if caffeine == 'True':
+        caffeine = True
+    else:
+        caffeine = False
+
     menstruation = request.form.get("menstruation")
-    print "menstruation", menstruation
+    if menstruation == 'True':
+        menstruation = True
+    else:
+        menstruation = False
+    
     bedtime = request.form.get("bedtime")
-    print "bedtime", bedtime, type(bedtime)
-    stress_level = request.form.get("stress_level")
-    print "stress", stress_level
-    activity_level = request.form.get("activity_level")
-    print "activity", activity_level
+    bedtime = datetime.strptime(bedtime, '%H:%M')
 
-    # new_entry = Entry(user_id=user_id,
-    #                     date=date,
-    #                     hours_sleep=hours_sleep,
-    #                     insomnia=insomnia,
-    #                     insom_type=insom_type,
-    #                     insom_severity=insom_severity,
-    #                     alcohol=alcohol,
-    #                     caffeine=caffeine,
-    #                     menstruation=menstruation,
-    #                     bedtime=bedtime,
-    #                     stress_level=stress_level,
-    #                     activity_level=activity_level)
+    stress_level = int(request.form.get("stress_level"))
 
-    # db.session.add(new_entry)
-    # db.session.commit()
+    activity_level = int(request.form.get("activity_level"))
+
+    new_entry = Entry(user_id=user_id,
+                        date=date,
+                        hours_sleep=hours_sleep,
+                        insomnia=insomnia,
+                        insom_type=insom_type,
+                        insom_severity=insom_severity,
+                        alcohol=alcohol,
+                        caffeine=caffeine,
+                        menstruation=menstruation,
+                        bedtime=bedtime,
+                        stress_level=stress_level,
+                        activity_level=activity_level)
+
+    db.session.add(new_entry)
+    db.session.commit()
 
 
     return render_template("dashboard.html")
@@ -76,7 +94,7 @@ def dashboard():
 if __name__ == "__main__":
 
     app.debug = True
-    #connect_to_db(app)
+    connect_to_db(app)
 
     DebugToolbarExtension(app)
 
