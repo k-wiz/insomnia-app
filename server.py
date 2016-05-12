4edd8a4b391675a7cdbee3a21a0b573d60cbabca
kelli <kwisuri@gmail.com>
2016-05-11 17:44:32 -0700

    Avg & median functions working and rendering correctly on dashboard.html

--- server.py
+++ server.py
@@ -5,9 +5,11 @@ from flask_debugtoolbar import DebugToolbarExtension
 from jinja2 import StrictUndefined
 from model import connect_to_db, db, User, Entry
 from datetime import datetime, time, date
+from sqlalchemy import func
+from helper import median, calculate_avg_sleep, calculate_avg_insom_severity, \
+                    calculate_median_sleep, calculate_median_insom_severity
 
 app = Flask(__name__)
-#WHAT IS THIS?
 app.secret_key = "DOESNTMATTER"
 app.jinja_env.undefined = StrictUndefined
 
@@ -26,12 +28,10 @@ def dashboard():
     """Display user's dashboard."""
 
 
-    #Uses form data to create new records in Entry table. 
+    # Uses form data to create new records in Entry table. 
 
     user_id = 1 #user_id hardcoded now; get id from session once login setup
-
     date = datetime.now()
-
     minutes_asleep = int((float(request.form.get("hours_sleep"))) * 60)
 
     insomnia = request.form.get("insomnia")
@@ -41,7 +41,6 @@ def dashboard():
         insomnia = False
 
     insom_type = request.form.get("insom_type")
-
     insom_severity = int(request.form.get("insom_severity"))
 
     alcohol = request.form.get("alcohol")
@@ -66,7 +65,6 @@ def dashboard():
     bedtime = datetime.strptime(bedtime, '%H:%M')
 
     stress_level = int(request.form.get("stress_level"))
-
     activity_level = int(request.form.get("activity_level"))
 
     new_entry = Entry(user_id=user_id,
@@ -85,15 +83,23 @@ def dashboard():
     db.session.add(new_entry)
     db.session.commit()
 
-    #Makes db queries to return insights on average hours asleep, median 
-    #hours asleep, average insomnia, median insomnia level, 
-    #(average activity level, average stress level).
-    avg_sleep = 
-    median_sleep = 
-    avg_insom_severity = 
-    median_insom_severity = 
+
+    # Calculate average sleep per night
+    avg_sleep = calculate_avg_sleep(user_id)
+
+    # Calculate average insomnia severity
+    avg_insom_severity = calculate_avg_insom_severity(user_id)
+
+    # Calculate median sleep per night
+    median_sleep = calculate_median_sleep(user_id)
+
+    # Calculate median insomnia severity
+    median_insom_severity = calculate_median_insom_severity(user_id)
 
 
+
+
+    # Pass data to template
     return render_template("dashboard.html", 
                                 avg_sleep=avg_sleep,
                                 median_sleep=median_sleep,
